238108bc1ff8b3b77a4d1c7ded744394a615e513
Mohammadsarairha <mohammadsarairha@gmail.com>
2022-03-15 02:09:48 +0300

    Add Number Game app

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void StartSequence()
+        {
+            Console.WriteLine("Please Enter a number greater than zero");
+            int prompt = Convert.ToInt32(Console.ReadLine());
+            while (prompt <= 0)
+            {
+                Console.WriteLine("Please Enter a number greater than zero");
+                prompt = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] userArray = new int[prompt];
+            int[] populateArr = Populate(userArray);
+            int sum = GetSum(populateArr);
+            int product = GetProduct(populateArr, sum);
+            decimal quotient = GetQuotient(product);
+            Console.WriteLine($"Your array is size {userArray.Length}");
+            Console.WriteLine($"The numbers in the array are {string.Join(",", populateArr)}");
+            Console.WriteLine($"The sum of the array is {sum}");
+            Console.WriteLine($"Quotient is {quotient}");
+        }
+        static int[] Populate(int[] arr)
+        {
+            int prompt = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number {i + 1} / {arr.Length}");
+                prompt = Int32.Parse(Console.ReadLine());
+                arr[i] = prompt;
+            }
+            return arr;
+        }
+        static int GetSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            if (sum < 20)
+            {
+                throw new Exception($"Value of {sum} is too low");
+            }
+            return sum;
+        }
+        static int GetProduct(int[] array, int sum)
+        {
+            Console.WriteLine($"Please select a random number between 1 and {array.Length}");
+            int random = Int32.Parse(Console.ReadLine());
+            int product = sum * array[random - 1];
+
+            return product;
+        }
+        static decimal GetQuotient(int product)
+        {
+            try
+            {
+                Console.WriteLine($"Please enter a number to divide ypur product {product} by");
+                decimal prompt = Decimal.Parse(Console.ReadLine());
+                decimal quotient = decimal.Divide(product, prompt);
+                return quotient;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                StartSequence();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something wrong happened : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("program is completed");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Lab.01a
+
+## Numbers Game App
+
+### Review 
+
+- This app is communicate with user and make user input what size his own array.
+- Push all element consider by array size and make it to chose random number (must number between 1 to array size ).
+- Random number is actually is index of array and take this number and multiply be summation of all array element.
+- Finally make user enter number and divide this number by result of (all array element * random / user number ).
+
+### What is the purpose of the program?
+- Conntrol of user input (always dont trust to user input)
+- Create dinamic app depends user input 
+- Divide your work (every function one job)
+
+### How do I run the program?
+- Just be calling one function in main function StartSequence().
+- Wait user input number .
+
+### What does the program look like?
+
+![Number game](./img/Number-game.png)
+
+
+
